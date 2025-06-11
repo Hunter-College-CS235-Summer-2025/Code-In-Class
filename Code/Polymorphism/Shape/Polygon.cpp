1b4237e0f1616d4d9a77dc3e5b5d2d6646dbe50d
Proto007 <sadab.hafiz52@myhunter.cuny.edu>
2025-06-11 13:18:33 -0400

    Updated formatting

--- Code/Polymorphism/Shape/Polygon.cpp
+++ Code/Polymorphism/Shape/Polygon.cpp
@@ -2,21 +2,27 @@
 
 Polygon::Polygon(size_t sides, float height, float width) : sides_(sides), height_(height), width_(width) {}
 
-size_t Polygon::getSides() const{
+size_t Polygon::getSides() const {
     return sides_;
 }
-float Polygon::getHeight() const{
+
+float Polygon::getHeight() const {
     return height_;
 }
-float Polygon::getWidth() const{
+
+float Polygon::getWidth() const {
     return width_;
 }
-void Polygon::setSides(size_t sides){
+
+void Polygon::setSides(size_t sides) {
     sides_ = sides;
 }
-void Polygon::setHeight(size_t height){
+
+void Polygon::setHeight(size_t height) {
     height_ = height;
 }
-void Polygon::setWidth(size_t width){
+
+void Polygon::setWidth(size_t width) {
     width_ = width;
 }
+
